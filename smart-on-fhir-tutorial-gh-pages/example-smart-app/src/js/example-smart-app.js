7b10f151d496ed84347bf113cf87e89fcb84f58d
jmplexi <hwangd476@gmail.com>
2019-10-20 22:45:51 -0700

    Update example-smart-app.js

--- smart-on-fhir-tutorial-gh-pages/example-smart-app/src/js/example-smart-app.js
+++ smart-on-fhir-tutorial-gh-pages/example-smart-app/src/js/example-smart-app.js
@@ -38,6 +38,7 @@
           }
 
           var height = byCodes('8302-2');
+          var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -49,6 +50,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
+          p.weight = getQuantityValueAndUnit(weight[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -122,7 +124,7 @@
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
-    $('#height').html(p.height);
+    $('#height').html(p.height + " Weight: " + p.weight);
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
